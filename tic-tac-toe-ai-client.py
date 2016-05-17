c28f641531cb92261ddc882d36ee1ab8e3614a32
busradeniz <busradeniz89@gmail.com>
2016-05-17 19:12:46 +0300

    print methods added

--- tic-tac-toe-ai-client.py
+++ tic-tac-toe-ai-client.py
@@ -1,4 +1,35 @@
 import xmlrpclib
+
+def print_board(board):
+
+    print "The board look like this: \n"
+
+        for i in range(3):
+            print " ",
+                for j in range(3):
+                    if board[i*3+j] == 1:
+                        print 'X',
+                        elif board[i*3+j] == 0:
+                            print 'O',
+                        elif board[i*3+j] != -1:
+                            print board[i*3+j]-1,
+                        else:
+                            print ' ',
+
+                        if j != 2:
+                            print " | ",
+                print
+
+                if i != 2:
+                    print "-----------------"
+                else:
+                    print
+
+def print_instruction():
+    print "Please use the following cell numbers to make your move"
+        print_board([2,3,4,5,6,7,8,9,10])
+
+
 def main():
     tic_tac_toe = xmlrpclib.ServerProxy('http://localhost:8000/tic-tac-toe')
     game_id = tic_tac_toe.new_game()
