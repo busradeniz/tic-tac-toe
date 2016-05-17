e28a317940f081b5a23254630d677ae34afd7c45
busradeniz <busradeniz89@gmail.com>
2016-05-17 18:50:32 +0300

    check_win API method implemented

--- tic-tac-toe-ai-server.py
+++ tic-tac-toe-ai-server.py
@@ -90,6 +90,21 @@ def _is_move_valid(board, index):
     return board[index] == -1
 
 
+def check_win(game_id):
+    board = games[game_id]
+    return _check_win(board)
+
+def _check_win(board):
+    win_cond = ((1,2,3),(4,5,6),(7,8,9),(1,4,7),(2,5,8),(3,6,9),(1,5,9),(3,5,7))
+    for each in win_cond:
+        try:
+            if board[each[0]-1] == board[each[1]-1] and board[each[1]-1] == board[each[2]-1]:
+                return board[each[0]-1]
+        except:
+            pass
+    return -1
+
+
 def main():
     # Create server
     server = SimpleXMLRPCServer(("localhost", 8000), requestHandler=RequestHandler, logRequests=True)
